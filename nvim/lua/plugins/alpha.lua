2affac6b06567ef1a4bc510b4338accafac3d059
Tae Kim <me@taehyoung.com>
2025-01-29 18:08:23 -0600

    update

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,9 +1,12 @@
 return {
   {
     "goolord/alpha-nvim",
-    dependencies = { "echasnovski/mini.icons" },
+    dependencies = {
+      "echasnovski/mini.icons",
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
-      require("alpha").setup(require("alpha.themes.startify").config)
+      require("alpha").setup(require("alpha.themes.theta").config)
     end,
   },
 }
